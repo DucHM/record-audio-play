cdcaedc87bae36cadb3ebc2b7bc070a255456f9b
Robert Hoang <mobile.apps.pro.vn@gmail.com>
2017-10-26 17:44:06 +0700

    first commit

---
+++ app/src/main/java/robert/com/recordandplayaudio/RecordAudioActivity.java
@@ -0,0 +1,108 @@
+package robert.com.recordandplayaudio;
+
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+public class RecordAudioActivity extends AppCompatActivity {
+    private final String TAG = "RecordAudioActivity";
+
+    Button play,stop,record;
+    private MediaRecorder myAudioRecorder;
+    private String outputFile = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(robert.com.recordandplayaudio.R.layout.activity_main);
+
+        play=(Button)findViewById(robert.com.recordandplayaudio.R.id.button3);
+        stop=(Button)findViewById(robert.com.recordandplayaudio.R.id.button2);
+        record=(Button)findViewById(robert.com.recordandplayaudio.R.id.button);
+
+        stop.setEnabled(false); // to disable button , it will enable after record button pressed
+        play.setEnabled(false);
+        Long tsLong = System.currentTimeMillis()/1000;
+        String ts = tsLong.toString();
+        outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/robert/recording-" + ts + ".3gp";
+        Log.e(TAG, "--->AudioPath=" + outputFile);
+
+        myAudioRecorder=new MediaRecorder();
+        myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        myAudioRecorder.setOutputFile(outputFile);
+
+
+        record.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    myAudioRecorder.prepare();
+                    myAudioRecorder.start();
+                } catch (IllegalStateException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+                record.setEnabled(false);
+                stop.setEnabled(true);
+
+                Toast.makeText(getApplicationContext(), "Recording started", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myAudioRecorder.stop();
+                myAudioRecorder.release();
+                myAudioRecorder  = null;
+
+                stop.setEnabled(false);
+                play.setEnabled(true);
+
+                Toast.makeText(getApplicationContext(), "Audio recorded successfully",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) throws IllegalArgumentException,SecurityException,IllegalStateException {
+                MediaPlayer m = new MediaPlayer();
+
+                try {
+                    m.setDataSource(outputFile);
+                }
+
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    m.prepare();
+                }
+
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                m.start();
+                Toast.makeText(getApplicationContext(), "Playing audio", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+    }
+}
