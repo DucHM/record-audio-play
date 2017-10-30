985c0cf1c8540e5d2ff9ea86521fa907bb629c55
VTM <mobile.apps.pro.vn@gmail.com>
2017-10-30 23:02:36 +0700

    Format file size

--- app/src/main/java/robert/com/recordandplayaudio/RecordingActivity.java
+++ app/src/main/java/robert/com/recordandplayaudio/RecordingActivity.java
@@ -24,6 +24,8 @@ import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 /**
  * Created by robert on 10/26/17.
@@ -165,12 +167,74 @@ public class RecordingActivity extends AppCompatActivity implements View.OnClick
                 if(fileSizeInByte <= 0) return "0KB";
                 final String[] units = new String[] { "B", "KB", "MB", "GB", "TB" };
                 int digitGroups = (int) (Math.log10(fileSizeInByte)/Math.log10(1024));
-                return new DecimalFormat("#,##0.#").format(fileSizeInByte/Math.pow(1024, digitGroups)) + " " + units[digitGroups];
+                return new DecimalFormat("#.##"/*"#,##0.#"*/).format(fileSizeInByte/Math.pow(1024, digitGroups)) + " " + units[digitGroups];
 
             }
         }
 
         return "0KB";
+    }
+
+    public static String humanReadableByteCount(long bytes, boolean si) {
+        int unit = si ? 1000 : 1024;
+        if (bytes < unit) return bytes + " B";
+        int exp = (int) (Math.log(bytes) / Math.log(unit));
+        String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp-1) + (si ? "" : "i");
+        return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
+    }
+
+    public static final double SPACE_KB = 1024;
+    public static final double SPACE_MB = 1024 * SPACE_KB;
+    public static final double SPACE_GB = 1024 * SPACE_MB;
+    public static final double SPACE_TB = 1024 * SPACE_GB;
+
+    public static String bytes2String(long sizeInBytes) {
+
+        NumberFormat nf = new DecimalFormat();
+        nf.setMaximumFractionDigits(2);
+
+        try {
+            if ( sizeInBytes < SPACE_KB ) {
+                return nf.format(sizeInBytes) + " Byte(s)";
+            } else if ( sizeInBytes < SPACE_MB ) {
+                return nf.format(sizeInBytes/SPACE_KB) + " KB";
+            } else if ( sizeInBytes < SPACE_GB ) {
+                return nf.format(sizeInBytes/SPACE_MB) + " MB";
+            } else if ( sizeInBytes < SPACE_TB ) {
+                return nf.format(sizeInBytes/SPACE_GB) + " GB";
+            } else {
+                return nf.format(sizeInBytes/SPACE_TB) + " TB";
+            }
+        } catch (Exception e) {
+            return sizeInBytes + " Byte(s)";
+        }
+
+    }/* end of bytes2String method */
+
+    public static void test() {
+
+        long sizeInBytes = 786;
+        System.out.println(sizeInBytes + " Bytes = " + bytes2String(sizeInBytes));
+
+        sizeInBytes = 456321;
+        System.out.println(sizeInBytes + " Bytes = " + bytes2String(sizeInBytes));
+
+        sizeInBytes = 896789489;
+        System.out.println(sizeInBytes + " Bytes = " + bytes2String(sizeInBytes));
+
+        sizeInBytes = 989678948985L;
+        System.out.println(sizeInBytes + " Bytes = " + bytes2String(sizeInBytes));
+
+        sizeInBytes = 1698296768946289482L;
+        System.out.println(sizeInBytes + " Bytes = " + bytes2String(sizeInBytes));
+
+        /**
+         786 Bytes = 786 Byte(s)
+         456321 Bytes = 445.63 KB
+         896789489 Bytes = 855.25 MB
+         989678948985 Bytes = 921.71 GB
+         1698296768946289482 Bytes = 1,544,591.91 TB
+         */
     }
 
     @Override
